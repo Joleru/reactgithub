e29b8bbbb7780fc5e5450e7d806fd1e3ce968b33
Joleru <jorgeleonardo30@gmail.com>
2025-08-06 19:31:27 -0500

    agregar componenten Menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,23 @@ function App() {
 
   return (
     <>
-     
+     <Menu />
     </>
   )
 }
 
 export default App
+
+function Menu(){
+
+  return(
+    <>
+      <h1>Menu</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </>
+  )
+}
